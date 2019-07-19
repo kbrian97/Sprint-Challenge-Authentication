9f33107599530d9bb6e01e0dc413b0e725ad13b0
kbrian97 <ktorgbede2015@fau.edu>
2019-07-18 21:17:45 -0400

    mvp

---
+++ answers.md
@@ -0,0 +1,15 @@
+ What is the purpose of using _sessions_?
+
+Sessions are a simple way to store data for individual users against a unique session ID. This can be used to persist state information between page requests. Session IDs are normally sent to the browser via session cookies and the ID is used to retrieve existing session data
+
+What does bcrypt do to help us store passwords in a secure manner.
+
+Bcrypt use hashing algorithm to make sure the passowrd are stored securely
+
+What does bcrypt do to slow down attackers?
+   
+With the hashing algorithm use salting to slow down attackers
+
+What are the three parts of the JSON Web Token?
+
+Header, payload, signature 
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt=require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const db  = require('../database/dbConfig.js')
+
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -10,10 +14,45 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password);
+  db("users")
+    .insert(user)
+    .then(id => {
+      res.status(201).send({ message: `id ${id} created`});
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+}
+function generateToken(user){
+  const payload = {
+    username: user.username,
+  }
+  const secret = process.env.JWT_SECRET
+  const options ={
+    expiresIn:'10m'
+  }
+  return jwt.sign(payload, secret, options)
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+  db("users")
+    .where("username", creds.username).first()
+    .then(user => { 
+    if( user && bcrypt.compareSync(creds.password, user.password)){
+      const token = generateToken(user);
+      res.status(202).json({msg:"user logged in", token});
+    }
+    else{
+      res.status(401).send("invalid username or password");
+    }
+    })
+    .catch(err =>{
+      res.status(500).send(err);
+    })
 }
 
 function getJokes(req, res) {
